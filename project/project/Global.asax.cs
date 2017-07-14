5d45f56c31e8f722d59b29e46ac6ba267df15fab
nguyen.lns <nguyen.lns@vn.gnt-global.com>
2017-07-14 13:13:25 +0700

    restore route

--- project/project/Global.asax.cs
+++ project/project/Global.asax.cs
@@ -14,7 +14,7 @@ namespace project
         void Application_Start(object sender, EventArgs e)
         {
             // Code that runs on application startup
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
+//            RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
